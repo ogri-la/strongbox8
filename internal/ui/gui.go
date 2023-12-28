25a00f75929aa16198855a53f12571712def6c11
Torkus <48141663+ogri-la@users.noreply.github.com>
2023-12-28 12:25:36 +1030

    tk experiments

--- internal/ui/gui.go
+++ internal/ui/gui.go
@@ -2,6 +2,7 @@ package ui
 
 import (
 	"bw/internal/core"
+	"fmt"
 
 	"log/slog"
 
@@ -12,16 +13,34 @@ type Window struct {
 	*tk.Window
 }
 
-func NewWindow() *Window {
+func NewWindow(app *core.App) *Window {
+
 	mw := &Window{tk.RootWindow()}
-	//lbl := tk.NewLabel(mw, "Hello ATK")
-	//btn := tk.NewButton(mw, "Quit")
+
+	mm := tk.NewMenu(mw)
+
+	//fm := tk.NewMenu(mw)
+	fm := mm.AddNewSubMenu("File")
+	fm.SetTearoff(false)
+
+	importcmd := tk.NewAction("Import")
+	importcmd.OnCommand(func() {
+		//tk.GetOpenFile(mw, "Open", []tk.FileType{}, "", "")
+		fmt.Println(tk.MainInterp().EvalAsStringList(`tk_getOpenFile`))
+	})
+
+	fm.AddAction(importcmd)
+
+	//fm := tk.NewAction("File")
+
+	//mm.AddAction(fm)
+	mw.SetMenu(mm)
+
 	//btn.OnCommand(func() {
 	//tk.Quit()
 	//})
-	//tk.NewVPackLayout(mw).AddWidgets(lbl, tk.NewLayoutSpacer(mw, 0, true), btn)
 
-	theme_widj := tk.NewComboBox(mw, &tk.WidgetAttr{"state", "readonly"}, &tk.WidgetAttr{"takeFocus", 0})
+	theme_widj := tk.NewComboBox(mw, &tk.WidgetAttr{"state", "readonly"})
 	theme_widj.SetValues(tk.TtkTheme.ThemeIdList())
 	theme_widj.SetCurrentText(tk.TtkTheme.ThemeId())
 	theme_widj.OnSelected(func() {
@@ -31,6 +50,7 @@ func NewWindow() *Window {
 	tabber := tk.NewNotebook(mw, tk.NotebookAttrWidth(20), tk.NotebookAttrHeight(20), tk.NotebookAttrTakeFocus(true))
 
 	mf := tk.NewFrame(mw)
+
 	mf2 := tk.NewFrame(mw)
 
 	// 'fill=both' seems to extend the widget horizontally
@@ -39,14 +59,54 @@ func NewWindow() *Window {
 	// - "Specifies whether the content should be expanded to consume extra space in their container."
 	//tk.Pack(tabber, &tk.LayoutAttr{"fill", "both"}, &tk.LayoutAttr{"expand", 1})
 
-	tk.Pack(tabber, &tk.LayoutAttr{"expand", 1}, &tk.LayoutAttr{"fill", "both"})
-
 	tabber.AddTab(mf, "main")
 
 	//disabled := tk.WidgetAttr{"state", "disabled"}
 
 	tabber.AddTab(mf2, "main 2") //, &disabled)
 	//defer tabber.Destroy()
+
+	tree := tk.NewTreeView(mf)
+
+	// figure out the bounds of the result set.
+	// for each result, test if $somemethod exists
+	// - if so, call that to figure out fields and labels, otherwise:
+
+	// for each result, determine the cells.
+	// - if struct, each field becomes a cell.
+	// - if list, each index becomes a cell
+	// - if primative, value is a single cell
+	// for each cell, determine a cell label
+	// - if struct, it's the field name
+	// - if list, its a counter
+	// - if primative, it's 0
+
+	// for each result, test if $somemethod exists to determine children.
+	// or, if a Result's item is a list of Results, then the first Item is the parent and the rest are children?
+	// - if so, call that then recursively do the above.
+
+	// the result should be a superset of all possible fields to display
+
+	tree.SetColumnCount(2)
+	tree.SetHeaderLabel(0, "id")
+	tree.SetHeaderLabel(1, "?")
+
+	for i, res := range app.ResultList() {
+
+		item := tree.InsertItem(nil, i, res.ID, []string{"foo"})
+		tree.InsertItem(item, 0, res.ID+"(child)", []string{"bar"})
+	}
+
+	tk.Pack(tree, &tk.LayoutAttr{"expand", 1}, &tk.LayoutAttr{"fill", "both"})
+
+	// ---
+
+	tk.Pack(tabber, &tk.LayoutAttr{"expand", 1}, &tk.LayoutAttr{"fill", "both"})
+
+	vpack2 := tk.NewVPackLayout(mf)
+	vpack2.AddWidget(tree)
+
+	// ---
 
 	vpack := tk.NewVPackLayout(mw)
 	vpack.AddWidget(theme_widj)
@@ -60,10 +120,14 @@ func StartGUI(app *core.App) {
 	tk.Init() // could this be problematic? is idempotent? without it the root window gets destroyed on quit
 
 	// https://ttkthemes.readthedocs.io/en/latest/loading.html#tcl-loading
-	tk.MainInterp().EvalAsStringList(`
+	fmt.Println(tk.MainInterp().EvalAsStringList(`
+set dir ttkfile/fsdialog
+source ttkfile/fsdialog/pkgIndex.tcl
+package require fsdialog
 source ttkthemes/ttkthemes/themes/pkgIndex.tcl
 source ttkthemes/ttkthemes/png/pkgIndex.tcl
-`)
+
+`))
 
 	slog.Debug("ttk theme list", "theme-list", tk.TtkTheme.ThemeIdList())
 	default_theme := "clearlooks"
@@ -72,7 +136,7 @@ source ttkthemes/ttkthemes/png/pkgIndex.tcl
 		slog.Warn("failed to set default theme", "default-theme", default_theme, "error", err)
 	}
 	tk.MainLoop(func() {
-		mw := NewWindow()
+		mw := NewWindow(app)
 		mw.SetTitle(app.KeyVal("bw", "app", "name"))
 		mw.Center(nil)
 		mw.ShowNormal()
