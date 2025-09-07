71f404c31a5487e464f95611356ae46935806c91
Torkus <48141663+ogri-la@users.noreply.github.com>
2025-09-07 11:44:29 +0930

    build-appimage.sh, generate a 'dist' directory with links to files to be distributed

--- build-appimage.sh
+++ build-appimage.sh
@@ -55,6 +55,26 @@ echo "--- upx"
     rm -rf ./squashfs-root
 )
 
+echo "--- uploadables"
+(
+    cd release
+    rm -rf dist
+    mkdir dist
+    (
+        cd dist
+        ln -s ../linux-amd64.upx linux-amd64
+        ln -s ../linux-amd64.upx.sha256 linux-amd64.sha256
+        ln -s ../linux-arm64 linux-arm64
+        ln -s ../linux-arm64.sha256 linux-arm64.sha256
+        ln -s ../strongbox.AppImage.upx strongbox.AppImage
+        ln -s ../strongbox.AppImage.upx.sha256 strongbox.AppImage.sha256
+        ln -s ../windows-amd64.exe.upx windows-amd64.exe
+        ln -s ../windows-amd64.exe.upx.sha256 windows-amd64.exe.sha256
+        ln -s ../windows-arm64.exe windows-arm64.exe
+        ln -s ../windows-arm64.exe.sha256 windows-arm64.exe.sha256
+    )
+)
+
 echo
 echo "--- cleaning up ---"
 rm -rf AppDir
