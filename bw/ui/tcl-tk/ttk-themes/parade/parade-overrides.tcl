db804b90210992509ba37a93139fddf54e83cd14
Torkus <48141663+ogri-la@users.noreply.github.com>
2025-10-12 23:49:31 +1030

    removed unused theme-editor code

--- bw/ui/tcl-tk/ttk-themes/parade/parade-overrides.tcl
+++ bw/ui/tcl-tk/ttk-themes/parade/parade-overrides.tcl
@@ -1,4 +1,4 @@
-# Parade theme overrides, generated Sun Oct 12 23:39:58 ACDT 2025
+# Parade theme overrides, generated Sun Oct 12 23:48:34 ACDT 2025
 # This file contains direct Tcl commands to apply theme customizations
 # These changes should be reconciled with the contents of parade.tcl
 
--- bw/ui/tcl-tk/ttk-themes/parade/parade.tcl
+++ bw/ui/tcl-tk/ttk-themes/parade/parade.tcl
@@ -63,9 +63,6 @@ namespace eval ttk::theme::parade {
             -selectforeground [list !focus white] \
             ;
 
-
-#        ttk::style configure Frame.border -relief groove
-
         ## Treeview.
         #
         ttk::style element create Treeheading.cell image \
@@ -77,7 +74,6 @@ namespace eval ttk::theme::parade {
                 ] \
             -border 4 -sticky ew
 
-        #ttk::style configure Treeview -fieldbackground white
         ttk::style configure Row -background "#efefef"
         ttk::style map Row -background [list \
                                        {focus selected} "#71869e" \
@@ -147,15 +143,6 @@ namespace eval ttk::theme::parade {
 
         ## Menubuttons.
         #
-        #ttk::style configure TMenubutton -relief raised -padding {10 2}
-# 	ttk::style element create Menubutton.border image $I(toolbutton-n) \
-# 	    -map [list \
-#                       pressed $I(toolbutton-p) \
-#                       selected $I(toolbutton-p) \
-#                       active $I(toolbutton-a) \
-#                       disabled $I(toolbutton-n)] \
-# 	    -border {4 7 4 7} -sticky nsew
-
         ttk::style element create Menubutton.border image \
              [list $I(button-n) \
                   selected $I(button-p) \
@@ -217,9 +204,6 @@ namespace eval ttk::theme::parade {
 
         ## Notebooks.
         #
-#         ttk::style element create tab image $I(tab-a) -border {2 2 2 0} \
-#             -map [list selected $I(tab-n)]
-
         ttk::style configure TNotebook.Tab -padding {15 5 15 5} -font {TkDefaultFont 12}
         ttk::style map TNotebook.Tab \
             -background [list selected $colors(-frame) {} $colors(-tabbg)] \
@@ -316,19 +300,8 @@ namespace eval ttk::theme::parade {
 
         ## Paned window parts.
         #
-#         ttk::style element create hsash image $I(hseparator-n) -border {2 0} \
-#             -map [list {active !disabled} $I(hseparator-a)]
-#         ttk::style element create vsash image $I(vseparator-n) -border {0 2} \
-#             -map [list {active !disabled} $I(vseparator-a)]
-
         ttk::style configure Sash -sashthickness 6 -gripcount 16
 
-
-        ## Separator.
-        #
-        #ttk::style element create separator image $I(sep-h)
-        #ttk::style element create hseparator image $I(sep-h)
-        #ttk::style element create vseparator image $I(sep-v)
 
         ## Treeview
         #
